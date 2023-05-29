e87243c6144f95a6b2039ed1b12caf8629a3d447
Sahil247-pixel <80456626+Sahil247-pixel@users.noreply.github.com>
2023-05-29 09:57:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919508247482', '𝛭𝑈𝛧𝛥𝛮', true],
+  ['919508247482', '𝛯𝑌𝛯𝛲𝛥𝑇𝐶𝛨', true], 
   [''] 
 ] //Number of owners
 
@@ -47,10 +47,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '𝛭𝑈𝛧𝛥𝛮'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𝛭𝑈𝛧𝛥𝛮┃𝛯𝑌𝛯𝛲𝛥𝑇𝐶𝛨' 
+global.author = '@EYEPATCH' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
